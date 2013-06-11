661093994aaad1e8a46173aef48dd810c90938dc
George Jenkins <joey@revelstone.org>
2013-06-11 21:03:05 +0000

    Add bootstrap. Add ask functionality to routes table and user query

--- rweb.rb
+++ rweb.rb
@@ -1,9 +1,39 @@
 require 'sinatra'
 require 'socket'
+require 'json'
+require 'pp'
+
+helpers do 
+    def ask(option)    
+        
+        s = TCPSocket.new '192.168.1.100', 4028
+        str = '{"command" : "'
+        unless option.nil?
+            str += "#{option}" + '"}'
+        else
+            str += 'summary"}'
+        end
+        s.puts str
+        json = s.gets
+        s.close
+        return JSON.parse(json.strip!)
+    end
+end
+
 get '/' do
     erb :index
 end
 
-get '/query' do
-    erb :query
+get '/index' do
+    erb :index
 end
+
+get '/query' do
+    erb :query, :locals => {:option  => params[nil]}
+end
+
+get '/query/:option' do
+    out = ask (params[:option])
+    "#{out}"
+end
+
